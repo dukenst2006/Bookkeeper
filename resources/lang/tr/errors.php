17eeb4aef98ad40ec3fe15017fb921b5b77c3b93
Egemen Berker Kızılcan <egemen@kenarkose.com>
2017-02-24 17:45:18 +0300

    Error pages

---
+++ resources/lang/tr/errors.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'be_right_back' => 'Hemen geri döneceğiz.',
+    'maintenance' => 'Web sitesini daha iyi hizmet verebilmek için bakıma aldık.',
+
+    'page_not_found' => 'Sayfa Bulunamadı.',
+    'we_could_not_find_the_page' => 'Aradığınız sayfayı bulamadık.',
+    'go_back_to_overview' => 'Genel görünüme geri dönün.',
+
+    'not_authorized' => 'Yetki Yok.',
+    'you_are_not_authorized' => 'Bu işlemi yapmak için gerekli yetkiye sahip değilsiniz.',
+
+    'bookkeeper_meltdown' => 'Bookkeeper Meltdown!',
+    'nobody_died' => 'Bir şeyler yolunda gitmedi... <br><i>Merak etmeyin kimse ölmedi!</i>'
+];
